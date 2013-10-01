2982e7cb7daf61af3faa662de690eac6da3a9da1
Bernalf <berfer88@gmail.com>
2013-10-01 15:43:20 -0600

    class 06

---
+++ js_jquery/class_06/module.js
@@ -0,0 +1,27 @@
+﻿///
+/// Lectura Recomendada http://www.etnassoft.com/2011/04/11/el-patron-de-modulo-en-javascript-en-profundidad/
+///
+
+// Namespace for the library
+var moduloName = {};
+
+// Library definition
+moduloName = (function () {
+    //Privates Vars  / properties
+    var options = {};
+
+    //Init Method
+    var initialize = function (opts) {
+        $.extend(options, opts);
+        methDummy(options.param1);
+    };
+
+    var methDummy = function (param1) {
+        console.log(param1 + options.param2);
+    };
+
+    // Public API - metodos publicos
+    return {
+        init: initialize
+    };
+})();
